bdab704e59d0553314c4a9212415dbdde8551135
Hussein Zaher <husseinzaher@outlook.com>
2020-05-07 13:53:35 +0200

    Add Class

---
+++ py_class.py
@@ -0,0 +1,13 @@
+class Calculator:
+    def __init__(self):
+        print("welcome in our calc")
+
+    def sum(self,x,y):
+        print(x+y)
+
+        def mul(self,x,y):
+            print(x*y)
+
+            my_calc = Calculator()
+            print(my_calc)
+            my_calc.sum(5,6)
--- py_objects.py
+++ py_objects.py
@@ -1,1 +1,6 @@
 ## everything is opject
+x = 5
+c = "HUSSEIN"
+print(type(x))
+print(type(c))
+
